2baa15c19ce0e737aff4f51026562f9f6fc321d4
linjialiang1234 <linjialiang86@163.com>
2018-12-30 14:55:27 +1100

    change format style

--- src/app/courses-page/course-item/createdDate.directive.ts
+++ src/app/courses-page/course-item/createdDate.directive.ts
@@ -16,17 +16,17 @@ export class CreatedDateDirective implements OnInit {
   	}
 
 	changeCourseBorderColor() {
-		let currentDate = new Date();
-		let validDateForFreshCourse = currentDate.setDate(currentDate.getDate()  -14);
+		const currentDate = new Date();
+		const validDateForFreshCourse = currentDate.setDate(currentDate.getDate() - 14);
 
 		if (this.creationDate.createDate < new Date() && this.creationDate.createDate >= validDateForFreshCourse) {
-			console.log("fresh course border is green")
+			console.log('fresh course border is green');
 			this.el.nativeElement.style.borderColor = 'green';
 		} else if (this.creationDate.createDate > new Date()) {
-			console.log("upcoming course border is blue")
+			console.log('upcoming course border is blue');
 			this.el.nativeElement.style.borderColor = 'blue';
 		} else {
-			console.log("nothing to change")
+			console.log('nothing to change');
 		}
 	}
 }
